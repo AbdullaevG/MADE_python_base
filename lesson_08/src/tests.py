43208aa18ea6dd388ddf9336b61d36825c285466
Gmurad735 <abd.gm90@yandex.ru>
2022-11-10 19:31:01 +0300

    add lesson_08

--- lesson_08/src/tests.py
+++ lesson_08/src/tests.py
@@ -1,6 +1,11 @@
 import unittest
 import random
+from unittest.mock import patch
+from io import StringIO
+
 from heap import heapsort
+import utils
+
 
 class TestHeapSort(unittest.TestCase):
     def setup(self):
@@ -20,4 +25,50 @@ class TestHeapSort(unittest.TestCase):
         random.shuffle(lst)
         self.assertEqual(actual, expected)
 
+
+class TesteMerge(unittest.TestCase):
+    def setup():
+        pass
+
+    
+    def test_merge2(self):
+        """ merge the same elements of lists"""
+        
+        with patch('sys.stdout', new=StringIO()):
+            lst1 = [1, 2, 4, 6, 7]
+            lst2 = [2, 3, 4, 7, 8, 10]
+
+            expected = [2, 4, 7]
+            actual = utils.merge(lst1, lst2)
+            self.assertEqual(actual, expected)
+
+            lst1 = [1, 2, 2, 4, 6, 7]
+            lst2 = [2, 2, 3, 4, 7, 8, 10]
+
+            expected = [2, 4, 7]
+            actual = utils.merge(lst1, lst2)
+            self.assertEqual(actual, expected)
+
+    def test_merge2_empty(self):
+        """Merge two equal lists"""
+
+        with patch('sys.stdout', new=StringIO()):
+            lst1 = []
+            lst2 = []
+
+            expected = []
+            actual = utils.merge(lst1, lst2)
+            self.assertEqual(actual, expected)
+
+
+    def test_mergek(self):
+        """ merge the same elements of from k lists"""
+
+        with patch('sys.stdout', new=StringIO()):
+            lists = [[1, 2, 4, 6, 7], \
+                     [2, 3, 4, 7, 8, 10]]
+
+            expected = [2, 4, 7]
+            actual = utils.merge_k(lists, len(lists))
+            self.assertEqual(actual, expected)
 

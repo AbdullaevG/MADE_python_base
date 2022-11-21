970b92e4be984180250d77b009048440dc4cf294
Gadzhimurad <abd.gm90@yandex.ru>
2022-11-21 12:34:54 +0300

    add another_test

---
+++ lesson_08/another_test/simple_for_test.py
@@ -0,0 +1,12 @@
+#!/bin/python
+
+
+def print_name():
+    name = input()
+    print(f"Your name is: {name}")
+
+def main():
+    print_name()
+
+if __name__ == "__main__":
+    main()
---
+++ lesson_08/another_test/test.py
@@ -0,0 +1,10 @@
+import unittest
+import random
+from unittest.mock import patch
+from io import StringIO
+
+from heap import heapsort
+import simple_for_test
+
+class TestSimple(unitt.TestCase):
+    def test_simple_for_test(self):
